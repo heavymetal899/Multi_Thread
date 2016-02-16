8eadecd81931e312bef9792acc4e72cf3c7c5518
heavymetal899 <heavymetal899@gmail.com>
2016-02-15 18:55:10 -0700

    Main Changes

--- app/src/main/java/com/example/steve/multithread/MainActivity.java
+++ app/src/main/java/com/example/steve/multithread/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.steve.multithread;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This app has buttons that create a file with the numbers 1-10 on separate lines,
+ * displays those numbers, and clears those numbers.
+ *
+ * @author Steve
+ */
 public class MainActivity extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
+    List<String> inputList = new ArrayList<String>();
+    private ProgressBar progress;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Set progress bar to the GUI progress bar
+        progress = (ProgressBar) findViewById(R.id.progressBar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +76,127 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This class handles creating the
+     * file and setting the progress.
+     *
+     */
+    class CreateAsync extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected void onPreExecute() {
+            progress.setProgress(0);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            String fileName = "numbers.txt";
+            String tempNum;
+
+            FileOutputStream outputStream;
+
+            try {
+                outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+                for (int i = 1; i < 11; i++) {
+                    tempNum = String.valueOf(i) + "\n";
+                    outputStream.write(tempNum.getBytes());
+                    publishProgress(i * 10);
+                    Thread.sleep(250);
+                }
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progress.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {}
+    }
+
+    /**
+     * This function calls the AsyncTask
+     * that creates a number file.
+     *
+     */
+    public void create(View v) {
+        new CreateAsync().execute();
+    }
+
+    /**
+     * This class handles displaying the
+     * file and setting the progress.
+     *
+     */
+    class LoadAsync extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected void onPreExecute() {
+            progress.setProgress(0);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            String fileName = "numbers.txt";
+
+            FileInputStream inputStream;
+            try {
+                inputStream = openFileInput(fileName);
+                InputStreamReader isr = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(isr);
+                String line;
+                int i = 1;
+                while ((line = bufferedReader.readLine()) != null) {
+                    inputList.add(line);
+                    publishProgress(i * 10);
+                    i++;
+                    Thread.sleep(250);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progress.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            ListView myListView = (ListView) findViewById(R.id.listView);
+
+            adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1,
+                    inputList);
+            myListView.setAdapter(adapter);
+        }
+    }
+
+    /**
+     * This function calls the AsyncTask
+     * that displays a number file.
+     *
+     */
+    public void load(View v) {
+        context = this;
+
+        new LoadAsync().execute();
+    }
+
+    /**
+     * This function calls clears
+     * the ListView.
+     *
+     */
+    public void clear(View v) {
+        adapter.clear();
+        ListView myListView = (ListView) findViewById(R.id.listView);
+        myListView.setAdapter(adapter);
     }
 }
